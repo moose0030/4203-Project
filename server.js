6a716f751d18f0fe724d595620d620d447ceaf5b
matthewpreston30 <matthewpreston@hotmail.ca>
2016-03-31 12:22:12 -0400

    simple server

--- server.js
+++ server.js
@@ -1,6 +1,33 @@
-var http = require('http')
 var port = process.env.PORT || 1337;
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World\n');
-}).listen(port);
+var fs = require('fs');
+var server = http.createServer( function(req, res) {
+
+    console.dir(req.param);
+
+    if (req.method == 'POST') {
+        console.log("POST");
+        var body = '';
+        req.on('data', function (data) {
+            body += data;
+            console.log("Partial body: " + body);
+        });
+        req.on('end', function () {
+            console.log("Body: " + body);
+        });
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end('post received');
+    }
+    else
+    {
+        console.log("GET");
+        //var html = '<html><body><form method="post" action="http://localhost:3000">Name: <input type="text" name="name" /><input type="submit" value="Submit" /></form></body>';
+        //var html = fs.readFileSync('index.html');
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end('');
+    }
+
+});
+
+var port = process.env.PORT || 1337;
+server.listen(port, host);
+console.log('Listening at http://' + host + ':' + port);
