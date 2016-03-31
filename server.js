dd554efb9d8e9874f6d9a7eac54ae806a0a599b1
matthewpreston30 <matthewpreston@hotmail.ca>
2016-03-31 13:48:00 -0400

    Eureka!

--- server.js
+++ server.js
@@ -1,9 +1,37 @@
-var http = require('http')
-var fs = require('fs');
-var port = process.env.PORT || 1337;
-var server = http.createServer( function(req, res) {
-res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World\n');
-}).listen(port);
+var app = require('http').createServer(handler)
+, io = require('socket.io').listen(app)
+, fs = require('fs')
 
-console.log('Listening on ' + port);
+app.listen(1337)
+
+
+
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+      function (err, data) {
+        if (err) {
+          res.writeHead(500);
+          return res.end('Error loading index.html');
+      }
+      res.writeHead(200);
+      res.end(data);
+  });
+  if(req.method == 'POST'){
+    console.log(req.method + 'It was a post...')
+    var body = '';
+    req.on('data', function (data) {
+        body += data;
+        console.log("Partial body: " + body);
+    });
+    req.on('end', function () {
+       console.log("Body: " + body);
+       io.emit('_post',body);
+   });
+
+}
+}
+
+
+
+
+console.log('Listening...');
