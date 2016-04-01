a0c8e2e46168d4a0016898ea276b39fd813d1ce3
matthewpreston30 <matthewpreston@hotmail.ca>
2016-03-31 20:52:44 -0400

    Added WeMo Motion

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ function handler (req, res) {
             case "RPI-WIFI":console.log(obj.type);io.emit('_post_rpi_wifi',obj); break;
             case "RPI-BT": console.log(obj.type);io.emit('_post_rpi_bluetooth',obj); break;
             case "WEMO-SWITCH": console.log(obj.type);io.emit('_post_wemo_switch',obj); break;
-            case "WEMO-MOTION":console.log(obj.type);break;
+            case "WEMO-MOTION":console.log(obj.type);io.emit('_post_wemo_motion',obj); break;
             default:  console.log(obj.type);io.emit('_post',body);
         }
     });
